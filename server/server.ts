93bf8bac26ea830c386076e640c3ba381cfa002e
rajdeep712 <rajdeeppa53@gmail.com>
2026-01-14 05:39:32 +0530

    Change origin in cors

--- server/server.ts
+++ server/server.ts
@@ -21,7 +21,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://thumblify-ai-thumbnail-generator.vercel.app",
+    ],
     credentials: true,
   })
 );
